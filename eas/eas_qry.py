bb97c98a019bd48578c6e62f7c8790a80a60876e
J C Gonzalez <JCGonzalez@sciops.esa.int>
2017-12-05 18:55:40 +0100

    Corrected typo in credits

--- eas/eas_qry.py
+++ eas/eas_qry.py
@@ -23,7 +23,7 @@ Usage:
 VERSION = '0.1.3'
 
 __author__ = "jcgonzalez" # Refactoring from ESDC Euclid Team code
-__credits__ = ["S,Nieto", "ESDC Euclid Team"]
+__credits__ = ["S.Nieto", "ESDC Euclid Team"]
 __version__ = VERSION
 __email__ = "jcgonzalez@sciops.esa.int"
 __status__ = "Prototype" # Prototype | Development | Production
